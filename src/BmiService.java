1d9459e504da7905830b4a51c70bb911f0e46c0b
Olga Oblat <oa.oblat@gmail.com>
2023-05-04 14:42:14 +0300

    changed files

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     public int calculate(int weightInKg, double heightInMeters) {
-        double bmi = weightInKg / (heightInMeters * heightInMeters);
+        double bmi = weightInKg / Math.pow(heightInMeters, 2);
         return (int) bmi;
     }
 }
